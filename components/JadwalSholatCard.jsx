c83b7483b954adf2f9f9e190307a60e49dd4913d
Fatih Arridho <71856084+FatihArridho@users.noreply.github.com>
2023-04-03 08:56:47 +0700

    .
    Signed-off-by: Fatih Arridho <71856084+FatihArridho@users.noreply.github.com>

--- components/JadwalSholatCard.jsx
+++ components/JadwalSholatCard.jsx
@@ -1,11 +1,11 @@
 export default function JadwalSholatCard({ sholat }) {
   const { name, time, active } = sholat;
 
-  const border = active ? "border border-rose-500" : "";
+  const border = active ? "border border-green-500" : "";
 
   return (
     <div
-      className={`rounded-lg shadow bg-rose-50 ${border} text-rose-500 flex justify-between items-center p-4`}
+      className={`rounded-lg shadow bg-green-50 ${border} text-green-500 flex justify-between items-center p-4`}
     >
       <div className="text-lg">{name}</div>
       <div className="font-bold flex items-center">
